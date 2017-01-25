4375f85794e6a547a04c966d5f4caae510fcb574
Puneeth G <puneeth019@gmail.com>
2017-01-25 22:16:35 +0530

    Update frequency.R

--- scripts/frequency.R
+++ scripts/frequency.R
@@ -15,14 +15,14 @@ files_list <- lapply(X = files_path, FUN = list.files)
 
 all_episodes <- mapply(FUN = function(x,y) paste0(x,y),
                        x = files_path, y=files_list) %>% 
-                unlist(use.names = F)
+  unlist(use.names = F)
 
 # Get total number of episodes in this T.V series
 num_episodes <- length(all_episodes)
 
 # Define a character vector for all six Lead characters
 # In Alphabetical order, trying to be unbiased :P
-friends <- c("chandler", "joey", "monica", "phoebe", "rachel", "ross")
+friends <- c("CHANDLER", "JOEY", "MONICA", "PHOEBE", "RACHEL", "ROSS")
 
 # Set colors to create plots/graphs
 plot_colors <- c("burlywood4", "tan2", "gold3", "brown", "green3", "gray")
@@ -35,26 +35,26 @@ num_dialogues <- matrix(data = NA, nrow = num_episodes, ncol = length(friends))
 
 # Run `for` loop on each episode to extract and plot data
 for(i in 1:num_episodes) {
-
+  
   file_url <- all_episodes[i]
-
+  
   all_Characters <- file_url %>%
     read_html() %>%
     html_nodes(css = 'b, p') %>%
     html_text()
-
+  
   # Define function `cleanlist`
   cleanlist <- function(x) gsub(pattern = "(.*):.*", 
                                 replacement = "\\1", x = x)
-
+  
   all_Characters <- sapply(X = all_Characters, FUN = cleanlist, USE.NAMES = F) %>% 
-                    str_trim() %>% 
-                    gsub(pattern = "^$", replacement = NA_character_) %>% 
-                    tolower()
+    str_trim() %>% 
+    gsub(pattern = "^$", replacement = NA_character_) %>% 
+    toupper()
   
   Lead_Characters <-
     all_Characters[all_Characters %in% friends]
-
+  
   # If data is extracted successfully for an episode, enter the 'if' section
   # If not, enter the 'else' section and display warning message
   if(sum(!is.na(x = Lead_Characters)) != 0) {
@@ -65,16 +65,16 @@ for(i in 1:num_episodes) {
         width = 800, height = 600)
     barplot(height = table(Lead_Characters), xlab = "Lead Characters",
             ylab = "#Dialogues", col = plot_colors,
-            main = "#Dialogues in F.R.I.E.N.D.S T.V series (1994-2004)",
+            main = "Number of Dialogues in F.R.I.E.N.D.S T.V series (1994-2004)",
             sub = paste0("Episode",i), font.sub = 3)
     dev.off()
-
-  } else warning(paste0("data scraping unsuccessful for episode ", i))
     
+  } else warning(paste0("data scraping unsuccessful for episode ", i))
+  
   # Store #Dialogues by each Lead character per episode in `num_dialogues`
   num_dialogues[i,] <- sapply(X = friends,
                               function(x) length(Lead_Characters[Lead_Characters %in% x]))
-                                
+  
   # Store the list of all repeated Lead character names for 
   # all episodes in a single character vector `Lead_Characters_allep`
   Lead_Characters_allep <- c(Lead_Characters_allep, Lead_Characters)
@@ -93,7 +93,7 @@ for(j in 1:length(friends)){
   png(paste0(WorkDir,"plots/#Dialogues_vs_ep_", friends[j],".png"), 
       width = 800, height = 600)
   plot(x = 1:num_episodes, y = num_dialogues[,j], type = "l",
-       main = paste0("#Dialogues vs. Episode by ", friends[j]) ,
+       main = paste0(" Number of Dialogues vs. Episode by ", friends[j]) ,
        xlab = "Episode Number", ylab = "#Dialogues in each episode",
        col = plot_colors[j], lwd = 3,
        xlim = c(1,num_episodes), ylim = c(0, 250))
@@ -107,7 +107,7 @@ for(j in 1:length(friends)){
 png(paste0(WorkDir,"plots/#Dialogues_vs_ep_allfriends.png"), 
     width = 800, height = 600)
 plot(x = 1:num_episodes, y = num_dialogues[,j], type = "l",
-     main = paste0("#Dialogues vs. Episode by ", friends[j]),
+     main = paste0("Number of Dialogues vs. Episode by ", friends[j]),
      xlab = "Episode Number", ylab = "#Dialogues in each episode",
      col = plot_colors[j], lwd = 3,
      xlim = c(1,num_episodes), ylim = c(0, 250))
@@ -122,6 +122,34 @@ png(paste0(WorkDir,"plots/allep_frequency_plot.png"),
     width = 800, height = 600)
 barplot(height=table(Lead_Characters_allep), xlab = "Lead Characters",
         ylab = "#Dialogues", col = plot_colors,
-        main = "#Dialogues in F.R.I.E.N.D.S T.V series (1994-2004)",
+        main = "Number of Dialogues in F.R.I.E.N.D.S T.V series (1994-2004)",
         sub = "Complete Series", font.sub = 3)
+dev.off()
+
+# Bar plot using `ggplot2` package
+# Convert the character vector `Lead_Characters_allep` 
+# into `data.frame` and change its column names
+df <- as.data.frame(table(Lead_Characters_allep))
+names(df) <- c("FRIENDS", "Number_of_Dialogues")
+# Set name and dimensions for the barplot
+png(paste0(WorkDir,"plots/allep_frequency_ggplot2.png"), 
+    width = 850, height = 550)
+p <- ggplot(data = df, aes(x = FRIENDS, y = Number_of_Dialogues, fill = FRIENDS)) +
+  # Set plot type to Bar plot and adjust width of bars
+  geom_bar(stat = "identity", width = 0.7) +
+  # Set theme to `minimal`
+  theme_minimal() +
+  # Set title for plot and lables for x & y axes
+  labs(title = "Total Number of Dialogues by Lead Characters
+       \n in F.R.I.E.N.D.S T.V series (1994-2004)", 
+       x = NULL, y = "Number of Dialogues") +
+  # Set Title and x & y axes tick mark labels
+  theme(plot.title = element_text(size = 20, hjust = 0.5), 
+        axis.text = element_text(face = "bold", size = 12), 
+        axis.title = element_text(face = "bold", size = 16),
+        #Remove legend
+        legend.position = "none") +
+  # Convert vertical barplot into horizontal one
+  coord_flip()
+p
 dev.off()

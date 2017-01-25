d4f3ba9e30f56feae655a8c672ad4cec28b25a71
Puneeth G <puneeth019@gmail.com>
2017-01-25 16:07:01 +0530

    Update frequency.R

--- scripts/frequency.R
+++ scripts/frequency.R
@@ -1,13 +1,14 @@
+# Load Packages
 load_pacakges <- c("dplyr", "tidyr", "readr", "stringr",
                    "magrittr", "ggplot2", "rvest", "lubridate", 
                    "rlist", "caret", "broom")
 lapply(load_pacakges, require, character.only = TRUE)
-# Load Packages
 
+# Set Working Directory
 WorkDir <- "C:/DA/Projects/F.R.I.E.N.D.S/"
 setwd(dir = paste0(WorkDir, "scripts/"))
-# Set Working Directory
 
+# Get the path of transcript files for all episodes
 season_num <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10")
 files_path <- paste0(WorkDir, "transcripts/season", season_num, "/")
 files_list <- lapply(X = files_path, FUN = list.files)
@@ -15,17 +16,24 @@ files_list <- lapply(X = files_path, FUN = list.files)
 all_episodes <- mapply(FUN = function(x,y) paste0(x,y),
                        x = files_path, y=files_list) %>% 
                 unlist(use.names = F)
+
+# Get total number of episodes in this T.V series
 num_episodes <- length(all_episodes)
 
-friends <- c("chandler", "joey", "monica",
-             "phoebe", "rachel", "ross")
-plot_colors <- c("burlywood4", "tan2", "gold3",
-                 "brown", "green3", "gray")
+# Define a character vector for all six Lead characters
+# In Alphabetical order, trying to be unbiased :P
+friends <- c("chandler", "joey", "monica", "phoebe", "rachel", "ross")
 
-Lead_Characters_allep <- NA # Initialize
+# Set colors to create plots/graphs
+plot_colors <- c("burlywood4", "tan2", "gold3", "brown", "green3", "gray")
 
+# Initialize vector `Lead_Characters_allep`
+Lead_Characters_allep <- NA 
+
+# Initialize matrix `num_dialogues`
 num_dialogues <- matrix(data = NA, nrow = num_episodes, ncol = length(friends))
 
+# Run `for` loop on each episode to extract and plot data
 for(i in 1:num_episodes) {
 
   file_url <- all_episodes[i]
@@ -47,50 +55,60 @@ for(i in 1:num_episodes) {
   Lead_Characters <-
     all_Characters[all_Characters %in% friends]
 
+  # If data is extracted successfully for an episode, enter the 'if' section
+  # If not, enter the 'else' section and display warning message
   if(sum(!is.na(x = Lead_Characters)) != 0) {
     
+    # Plot #Dialogues vs. Lead Character for each episode
+    # One Bar Plot per loop
     png(paste0(WorkDir,"plots/episode", i, "frequency_plot.png"),
         width = 800, height = 600)
-    barplot(height=table(Lead_Characters), xlab = "Lead Characters",
-            ylab = "Number of Dialogues", col = plot_colors,
-            main = "Number of Dialogues in F.R.I.E.N.D.S TV series (1994-2004)",
+    barplot(height = table(Lead_Characters), xlab = "Lead Characters",
+            ylab = "#Dialogues", col = plot_colors,
+            main = "#Dialogues in F.R.I.E.N.D.S T.V series (1994-2004)",
             sub = paste0("Episode",i), font.sub = 3)
     dev.off()
 
-  } else warning(paste0("fail for", i))
+  } else warning(paste0("data scraping unsuccessful for episode ", i))
     
-  # Number of dialogues by each character in each episode
+  # Store #Dialogues by each Lead character per episode in `num_dialogues`
   num_dialogues[i,] <- sapply(X = friends,
                               function(x) length(Lead_Characters[Lead_Characters %in% x]))
                                 
-  # Store all Lead Characters in single character vector
+  # Store the list of all repeated Lead character names for 
+  # all episodes in a single character vector `Lead_Characters_allep`
   Lead_Characters_allep <- c(Lead_Characters_allep, Lead_Characters)
   
 }
 
-# Convert matrix `num_dialogues` into data.frame
-# and name its columns
+# Convert matrix `num_dialogues` into data.frame and name its columns
 num_dialogues <- as.data.frame(x = num_dialogues)
 names(num_dialogues) <- friends
 
-# Plot number of dialogues versus episode for each friend
+# Plot `#Dialogues` vs. `Episode number` for each Lead Character
+# This `for` loop runs six times, once for each Lead character
+# Total six line plots, one per loop
 for(j in 1:length(friends)){
   
-  png(paste0(WorkDir,"plots/#dial_vs_ep_", friends[j],".png"), width = 800, height = 600)
+  png(paste0(WorkDir,"plots/#Dialogues_vs_ep_", friends[j],".png"), 
+      width = 800, height = 600)
   plot(x = 1:num_episodes, y = num_dialogues[,j], type = "l",
-       main = paste0("Number of Dialogues vs. Episode by ", friends[j]) ,
-       xlab = "Episode Number", ylab = "Number of Dialogues in each episode",
+       main = paste0("#Dialogues vs. Episode by ", friends[j]) ,
+       xlab = "Episode Number", ylab = "#Dialogues in each episode",
        col = plot_colors[j], lwd = 3,
        xlim = c(1,num_episodes), ylim = c(0, 250))
   dev.off()
   
 }
 
-# Plot number of dialogues versus episode for each friend in the same plot
-png(paste0(WorkDir,"plots/#dial_vs_ep_allfriends.png"), width = 800, height = 600)
+# Plot `#Dialogues` versus `Episode number` for all 
+# six Lead characters in the same plot, unlike above plot
+# Six line plots in single Chart
+png(paste0(WorkDir,"plots/#Dialogues_vs_ep_allfriends.png"), 
+    width = 800, height = 600)
 plot(x = 1:num_episodes, y = num_dialogues[,j], type = "l",
-     main = paste0("Number of Dialogues vs. Episode by ", friends[j]),
-     xlab = "Episode Number", ylab = "Number of Dialogues in each episode",
+     main = paste0("#Dialogues vs. Episode by ", friends[j]),
+     xlab = "Episode Number", ylab = "#Dialogues in each episode",
      col = plot_colors[j], lwd = 3,
      xlim = c(1,num_episodes), ylim = c(0, 250))
 
@@ -98,10 +116,12 @@ for(k in 2:length(friends))
   lines(x = 1:num_episodes, y = num_dialogues[,k], col = plot_colors[k])
 dev.off()
 
-# Plot number of dialogues summed for all episodes versus friend
-png(paste0(WorkDir,"plots/allep_frequency_plot.png"), width = 800, height = 600)
+# Plot #Dialogues summed for all episodes vs. each Lead Character
+# Single Bar Plot
+png(paste0(WorkDir,"plots/allep_frequency_plot.png"), 
+    width = 800, height = 600)
 barplot(height=table(Lead_Characters_allep), xlab = "Lead Characters",
-        ylab = "Number of Dialogues", col = plot_colors,
-        main = "Number of Dialogues in F.R.I.E.N.D.S TV series (1994-2004)",
+        ylab = "#Dialogues", col = plot_colors,
+        main = "#Dialogues in F.R.I.E.N.D.S T.V series (1994-2004)",
         sub = "Complete Series", font.sub = 3)
 dev.off()

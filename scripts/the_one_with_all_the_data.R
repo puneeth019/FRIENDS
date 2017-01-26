e1e18244989267f43e410f717d0b7905226f3c41
Puneeth G <puneeth019@gmail.com>
2017-01-26 17:11:06 +0530

    Update the_one_with_all_the_data.R

--- scripts/the_one_with_all_the_data.R
+++ scripts/the_one_with_all_the_data.R
@@ -22,13 +22,13 @@ all_episodes <- mapply(FUN = function(x,y) paste0(x,y),
 num_episodes <- length(all_episodes)
 
 # Define a character vector for all six Lead characters
-# In Alphabetical order, trying to be unbiased :P
+# In Alphabetical order, trying to be unbiased :D
 FRIENDS <- c("CHANDLER", "JOEY", "MONICA",
              "PHOEBE", "RACHEL", "ROSS")
 
-# Set colors to create plots/graphs
-# These are color-blind-friendly palettes
-plot_colors <- c("#000000", "#E69F00", "#56B4E9",
+# Set colours to create plots/graphs
+# These are colour-blind-friendly palettes
+plot_colours <- c("#000000", "#E69F00", "#56B4E9",
                  "#009E73", "#F0E442", "#0072B2")
 
 # Initialize vector `Lead_Characters_allep`
@@ -65,19 +65,7 @@ for(i in 1:num_episodes) {
   # If not, enter the 'else' section and display warning message
   if(sum(!is.na(x = Lead_Characters)) != 0) {
     
-    # Plot #Dialogues vs. Lead Character for each episode
-    # One Bar Plot per loop
-    #png(filename = paste0(WorkDir,"plots/episode", i, "frequency_plot.png"),
-    #    width = 800, height = 500)
-    #barplot(height = table(Lead_Characters), xlab = "Lead Characters",
-    #        ylab = "Number of Dialogues", col = plot_colors,
-    #        main = "Number of Dialogues in 
-    #        F.R.I.E.N.D.S T.V series (1994-2004)",
-    #        sub = paste0("Episode #",i), font.sub = 3)
-    #dev.off()
     
-    
-    # Bar plot using `ggplot2`
     
     # Convert the character vector `Lead_Characters` 
     # into `data.frame` and change its column names
@@ -85,7 +73,7 @@ for(i in 1:num_episodes) {
     names(df) <- c("FRIENDS", "Number_of_Dialogues")
     # Set name and dimensions for the plot
     png(paste0(WorkDir,"plots/episode",
-               i, "frequency_ggplot2.png"),
+               i, "frequency_plot.png"),
         width = 800, height = 500)
     p <- ggplot(data = df, aes(x = FRIENDS, 
                                y = Number_of_Dialogues,
@@ -106,22 +94,26 @@ for(i in 1:num_episodes) {
             axis.title.y = element_text(vjust = 1.5),
             # legend position
             legend.position = "none") +
-      # Set colors for bars
-      scale_fill_manual(values = plot_colors) +
+      # Set colours for bars
+      scale_fill_manual(values = plot_colours) +
       # Convert vertical plot into horizontal one
       coord_flip()
     print(p)
     dev.off()
     
-  } else warning(paste0("data scraping unsuccessful for episode #", i))
+  } else warning(paste0("data scraping 
+                        unsuccessful for episode #", i))
   
   # Store #Dialogues by each Lead character per episode in `num_dialogues`
   num_dialogues[i,] <- sapply(X = FRIENDS,
-                              function(x) length(Lead_Characters[Lead_Characters %in% x]))
+                              function(x) 
+                                length(Lead_Characters[Lead_Characters %in% x]))
   
-  # Store the list of all repeated Lead character names for 
-  # all episodes in a single character vector `Lead_Characters_allep`
-  Lead_Characters_allep <- c(Lead_Characters_allep, Lead_Characters)
+  # Store the list of all repeated Lead character 
+  # names for all episodes in a single character 
+  # vector `Lead_Characters_allep`
+  Lead_Characters_allep <- c(Lead_Characters_allep, 
+                             Lead_Characters)
   
 }
 
@@ -129,44 +121,30 @@ for(i in 1:num_episodes) {
 num_dialogues <- as.data.frame(x = num_dialogues)
 names(num_dialogues) <- FRIENDS
 
-# Plot `#Dialogues` vs. `Episode number` for each Lead Character
-# This `for` loop runs six times, once for each Lead character
-# Total six line plots, one per loop
-for(j in 1:length(FRIENDS)){
-  
-  png(filename = paste0(WorkDir,"plots/#Dialogues_vs_ep_", FRIENDS[j],".png"), 
-      width = 800, height = 500)
-  plot(x = 1:num_episodes, y = num_dialogues[,j], type = "l",
-       main = paste0(" Number of Dialogues vs. Episode by ", FRIENDS[j]),
-       xlab = "Episode Number", ylab = "Number of Dialogues",
-       col = plot_colors[j], lwd = 3,
-       xlim = c(1,num_episodes), ylim = c(0, 250))
-  dev.off()
-  
-}
 
 
-# Line plot using `ggplot2`
 
+
+## Plot `#Dialogues` vs. `Episode number` for each Lead Character
+## This `for` loop runs six times, once for each Lead character
+## Total six line plots, one per loop
 # Create data.frame `df` and change its column names
 df <- data.frame(1:num_episodes, num_dialogues)
 names(df) <- c("Episode_Number", FRIENDS)
-# Plot `#Dialogues` vs. `Episode number` for each Lead Character
-# This `for` loop runs six times, once for each Lead character
-# Total six line plots, one per loop
 for(j in 1:length(FRIENDS)){
   
   # Set name and dimensions for the plot
   png(filename = paste0(WorkDir,
                         "plots/#Dialogues_vs_ep_", FRIENDS[j],".png"),
       width = 800, height = 500)
-  p <- ggplot(data = df, aes(x = Episode_Number, y = CHANDLER)) +
+  p <- ggplot(data = df, aes(x = Episode_Number, y = df[,j+1])) +
     # Set plot type to line plot
     geom_line() +
     # Set theme to `minimal`
     theme_minimal() +
     # Set title for plot and lables for x & y axes
-    labs(title = paste0(" Number of Dialogues vs. Episode by ", FRIENDS[j]),
+    labs(title = paste0(" Number of Dialogues vs. Episode by ",
+                        FRIENDS[j]),
          x = "Episode Number", y = "Number of Dialogues") +
     # Set text for Title, x & y axes labels
     theme(plot.title = element_text(size = 20, hjust = 0.5), 
@@ -174,46 +152,60 @@ for(j in 1:length(FRIENDS)){
           axis.title = element_text(face = "bold", size = 16),
           axis.title.y = element_text(vjust = 1.5),
           # legend position
-          legend.position = "none") +
-  # Print the plot
-  print(p)
+          legend.position = "none")
+    # Print the plot
+    print(p)
   dev.off()
-
+  
 }
 
-# Plot `#Dialogues` versus `Episode number` for all 
-# six Lead characters in the same plot, unlike above plot
-# Six line plots in single Chart
-png(filename = paste0(WorkDir,
-                      "plots/#Dialogues_vs_ep_allfriends.png"), 
-    width = 800, height = 500)
-plot(x = 1:num_episodes, y = num_dialogues[,j], type = "l",
-     main = paste0("Number of Dialogues vs. Episode by ", FRIENDS[j]),
-     xlab = "Episode Number", ylab = "#Dialogues in each episode",
-     col = plot_colors[j], lwd = 3,
-     xlim = c(1,num_episodes), ylim = c(0, 250))
 
-for(k in 2:length(FRIENDS))
-  lines(x = 1:num_episodes, y = num_dialogues[,k], col = plot_colors[k])
+
+
+## Plot `#Dialogues` versus `Episode number` for all 
+## six Lead characters in the same plot, unlike above plot
+## Six line plots in a single Chart
+# Set name and dimensions for the plot
+png(paste0(WorkDir, "plots/#Dialogues_vs_ep_allfriends.png"), 
+    width = 800, height = 500)
+p <- ggplot(data = df, aes(x = Episode_Number)) +
+  geom_line(aes(y = df[,2], colour = FRIENDS[1])) + 
+  geom_line(aes(y = df[,3], colour = FRIENDS[2])) +
+  geom_line(aes(y = df[,4], colour = FRIENDS[3])) + 
+  geom_line(aes(y = df[,5], colour = FRIENDS[4])) +
+  geom_line(aes(y = df[,6], colour = FRIENDS[5])) +
+  geom_line(aes(y = df[,7], colour = FRIENDS[6])) +
+  # Set colours for lines manually using `plot_colours`
+  scale_colour_manual("FRIENDS", values = plot_colours) +
+  # Set theme to `minimal`
+  theme_minimal() +
+  # Set title for plot and lables for x & y axes
+  labs(title = paste0("Number of Dialogues versus Episode Number"), 
+       x = "Episode Number", y = "Number of Dialogues") +
+  # Set text for Title, x & y axes labels
+  theme(plot.title = element_text(size = 20, hjust = 0.5), 
+        axis.text = element_text(face = "bold", size = 12),
+        axis.title = element_text(face = "bold", size = 16),
+        axis.title.y = element_text(vjust = 1.5),
+        # legend position
+        legend.position = c(0.1,0.8))
+# Print the plot
+print(p)
 dev.off()
 
-# Plot #Dialogues summed for all episodes vs. each Lead Character
-# Single Bar Plot
-#png(filename = paste0(WorkDir,"plots/allep_frequency_plot.png"), 
-#    width = 800, height = 500)
-#barplot(height=table(Lead_Characters_allep), xlab = "Lead Characters",
-#        ylab = "#Dialogues", col = plot_colors,
-#        main = "Number of Dialogues in F.R.I.E.N.D.S T.V series (1994-2004)",
-#        sub = "Complete Series", font.sub = 3)
-#dev.off()
 
-# Bar plot using `ggplot2` package
+
+
+
+## Plot `Total #Dialogues` vs. `Episode number` 
+## summed for all episodes for all Lead Character
+## Single barplot
 # Convert the character vector `Lead_Characters_allep` 
 # into `data.frame` and change its column names
 df <- as.data.frame(table(Lead_Characters_allep))
 names(df) <- c("FRIENDS", "Number_of_Dialogues")
 # Set name and dimensions for the plot
-png(paste0(WorkDir,"plots/allep_frequency_ggplot2.png"),
+png(paste0(WorkDir,"plots/allep_frequency_plot.png"),
     width = 800, height = 500)
 p <- ggplot(data = df, aes(x = FRIENDS,
                            y = Number_of_Dialogues,
@@ -232,10 +224,12 @@ p <- ggplot(data = df, aes(x = FRIENDS,
         axis.title = element_text(face = "bold", size = 16),
         # legend position
         legend.position = "none") +
-  # Set colors for bars
-  scale_fill_manual(values = plot_colors) +
+  # Set colours for bars
+  scale_fill_manual(values = plot_colours) +
   # Convert vertical barplot into horizontal one
   coord_flip()
 # Print the plot
 print(p)
 dev.off()
+
+
